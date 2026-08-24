392187bd4d2af1b024cc728d81d3786123fd9bd5
Jake D <jakedryka1@gmail.com>
2026-08-24 17:50:30 +0800

    Mount App Store Safety Center

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AppStoreSafety from './AppStoreSafety.jsx'
 import ErrorBoundary from './ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <AppStoreSafety />
     </ErrorBoundary>
   </StrictMode>,
 )
